15c0ecf956563d19cf88959042f36f2111842168
Gratian Crisan <gratian.crisan@ni.com>
2023-12-15 16:21:08 -0600

    kernel-containerized-performance-tests: run 2 cyclictest containers in parallel
    @todo this is work-in-progress
    
    @todo description
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 source "$(dirname "$0")"/common.cfg
 
+CPUS=`nproc --all`
+if [ $CPUS -lt 2]; then
+	echo "ERROR: the containerized hackbench test requires a system with at least 2 CPUs"
+	echo "SKIP: test_kernel_hackbench_containerized"
+	exit 77
+fi
+
 # Start background scheduler load
 LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -v ${LOG_DIR}:${LOG_DIR} \
@@ -9,8 +16,13 @@ LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -t parallel-container)
 docker exec -d $LOAD_CONT "$TEST_DIR/hackbench-load" "start"
 
-# Run cyclictest
-"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized"
+# Run cyclictest containers on the last 2 CPUs on the system
+
+# @todo: this is currently blocking so the second container won't start until the fist one is done
+"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized" $((CPUS - 1))
+
+# @todo: this will overwrite the test result from the previous container
+"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized" $((CPUS - 2))
 
 # Clean up the background container
 docker exec $LOAD_CONT "$TEST_DIR/hackbench-load" "stop"
